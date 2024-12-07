a758e609fbd13adf064d8e8c5a15988537ddada0
강민재 <minijae011030@naver.com>
2024-12-07 19:47:14 +0900

    14-2 bellmanFord algorithm 주석 추가

--- 14-2.c
+++ 14-2.c
@@ -33,6 +33,7 @@ int main() {
 
     BellmanFord(G, &G.vertices[G.start_index]);
 
+    // 시작 정점에서 다른 정점들까지의 거리 출력
     if (G.vertices[G.start_index].out_degree != 0) {
         for (int i = 0; i < G.vertice_num; i++) {
             if (i != G.start_index && G.vertices[i].distance != 30000) {
@@ -70,21 +71,27 @@ Graph initGraph() {
     return G;
 }
 
+// 벨먼-포드 알고리즘
 void BellmanFord(Graph G, Vertices* start_vertice) {
+    // 모든 정점의 초기 거리 설정
     for (int i = 0; i < G.vertice_num; i++) {
-        G.vertices[i].distance = 30000;
+        G.vertices[i].distance = 30000; // 초기 거리를 매우 큰 값으로 설정
     }
 
+    // 시작 정점의 거리 0으로 설정
     start_vertice->distance = 0;
 
+    // 정점 개수 - 1 만큼 반복
+    // 각 정점에서 간선을 통해 최단 거리를 반복적으로 업데이트
     for (int i = 0; i < G.vertice_num - 1; i++) {
         for (int j = 0; j < G.edge_num; j++) {
-            Vertices* u = &G.vertices[G.edge[j].from_index];
-            Vertices* z = &G.vertices[G.edge[j].to_index];
-            int weight = G.edge[j].weight;
+            Vertices* u = &G.vertices[G.edge[j].from_index]; // 간선의 시작 정점
+            Vertices* z = &G.vertices[G.edge[j].to_index];   // 간선의 도착 정점
+            int weight = G.edge[j].weight;                  // 간선 가중치
 
+            // 시작 정점으로부터의 거리가 유효하고, 현재 계산된 거리보다 작다면 업데이트
             if (u->distance != 30000 && u->distance + weight < z->distance) {
-                z->distance = u->distance + weight;
+                z->distance = u->distance + weight; // 거리 갱신
             }
         }
     }
